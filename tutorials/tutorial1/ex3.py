01902d8a45f72e0f41fbf135c066928b436c0438
Seymour Aaron <seymour@student34.strw.leidenuniv.nl>
2019-02-26 01:22:36 +0100

    updated interpolation

--- tutorials/tutorial1/ex3.py
+++ tutorials/tutorial1/ex3.py
@@ -1,14 +1,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x=np.linspace(1,20,7)
-f=2*x*np.sin(0.8*x)
+basedata=np.linspace(1,20,7)
 
-print(f)
 
-interpvals=np.linspace(1,25,100)
+def fx(data):
+    return 2*data*np.sin(0.8*data)
 
-for pt in x:
-    e1=
-    e2=
-    mdpt=
+
+
+sample_points=np.linspace(1,25,100)
+
+#find the points in the sample points mesh that lie adjacent to each of the data
+# points. I think in this case that will be M=3 points per data point in the
+# sample point space.
+# One to the left of the data point, the data point itself, and the one
+# right of it. "j_lo" is that the first of those: the left one.
+
+
+j_low=[]
+for x in basedata:
+    interp_point=np.argmin(abs(x-sample_points))
+    j_low.append(  interp_point  )
+j_low=np.asarray(j_low)
+
+
+xinterp=sample_points[ j_low[:].astype(int) ]
+ydata=fx(xinterp)
+
+plt.plot(basedata,np.zeros(len(basedata)),'ro')
+plt.plot(xinterp,np.zeros(len(xinterp)),'bo')
+plt.plot(xinterp,ydata,'yo')
+
+
+#linear interpolate on these values:
+interpolation=[]
+for i in range(len(xinterp)):
+    print "section",i+1,"/",len(xinterp)
+    if i<len(xinterp)-1:
+        print "from:",min(sample_points[j_low[i]:j_low[i+1]]),"to:",max(sample_points[j_low[i]:j_low[i+1]])
+        y=ydata[i]+(sample_points[j_low[i]:j_low[i+1]]-xinterp[i])*( (ydata[i+1]-ydata[i]) / (xinterp[i+1]-xinterp[i]) )
+        interpolation.append(y)
+    else:
+        print "(must extrapolate)"
+        print i
+        #y=ydata[i-1]+(sample_points[j_low[i]:]-xinterp[i-1])*( (ydata[i]-ydata[i-1]) / (xinterp[i]-xinterp[i-1]) )
+interpolation=np.asarray(interpolation)
+interpolation=interpolation.ravel()
+
+print sample_points.shape,len(interpolation)
+
+plt.plot(sample_points[:len(interpolation)],interpolation,'g^')
+plt.show()
+
+
+"""  
+# do this via bisection algorithm:
+
+    #loop over each data point  
+    #set edge points in the sample_points initially to the min and max
+    edgelow=min(sample_points)
+    edgehigh=max(sample_points)
+    #find the mid point of this by len/2
+    midpoint=(edgelow+edgehigh)/2
+    #check if the data point is < or > that mid point
+    if d<midpoint:
+        edgehigh=midpoint
+    #reset edge point(s) accordingly (move min to half, or max to half)
+    #set new mid point, and check where data point is again.
+    #repeat until data point either is a sample point OR is in btwn two sample points.
+"""
+
+
+
